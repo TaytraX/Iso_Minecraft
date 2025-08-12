fc367d2f35e3778e410c01de833fc0cf7dac1208
TaytraX <gregoirengilot@gmail.com>
2025-08-12 15:54:57 -0400

    the base Import LWJGL native librarie liaison moduls with racine

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,38 @@ plugins {
 group = "Core"
 version = "1.0-SNAPSHOT"
 
+val lwjglVersion = "3.3.6"
+val jomlVersion = "1.10.8"
+val `joml-primitivesVersion` = "1.10.0"
+
+val lwjglNatives = Pair(
+    System.getProperty("os.name")!!,
+    System.getProperty("os.arch")!!
+).let { (name, arch) ->
+    when {
+        "FreeBSD".equals(name)                                    ->
+            "natives-freebsd"
+        arrayOf("Linux", "SunOS", "Unit").any { name.startsWith(it) } ->
+            if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
+                "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
+            else if (arch.startsWith("ppc"))
+                "natives-linux-ppc64le"
+            else if (arch.startsWith("riscv"))
+                "natives-linux-riscv64"
+            else
+                "natives-linux"
+        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) }     ->
+            "natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+        arrayOf("Windows").any { name.startsWith(it) }                ->
+            if (arch.contains("64"))
+                "natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+            else
+                "natives-windows-x86"
+        else                                                                            ->
+            throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
+    }
+}
+
 repositories {
     mavenCentral()
 }
@@ -12,6 +44,54 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-fmod")
+    implementation("org.lwjgl", "lwjgl-freetype")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-jemalloc")
+    implementation("org.lwjgl", "lwjgl-ktx")
+    implementation("org.lwjgl", "lwjgl-libdivide")
+    implementation("org.lwjgl", "lwjgl-llvm")
+    implementation("org.lwjgl", "lwjgl-lmdb")
+    implementation("org.lwjgl", "lwjgl-lz4")
+    implementation("org.lwjgl", "lwjgl-nuklear")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-opus")
+    implementation("org.lwjgl", "lwjgl-shaderc")
+    implementation("org.lwjgl", "lwjgl-spvc")
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation("org.lwjgl", "lwjgl-tinyfd")
+    implementation("org.lwjgl", "lwjgl-tootle")
+    implementation("org.lwjgl", "lwjgl-vma")
+    implementation("org.lwjgl", "lwjgl-vulkan")
+    implementation ("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-freetype", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-jemalloc", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-ktx", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-libdivide", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-llvm", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-lmdb", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-lz4", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-nuklear", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-opus", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-shaderc", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-spvc", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-tinyfd", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-tootle", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-vma", classifier = lwjglNatives)
+    if (lwjglNatives == "natives-macos" || lwjglNatives == "natives-macos-arm64") implementation ("org.lwjgl", "lwjgl-vulkan", classifier = lwjglNatives)
+    implementation("org.joml", "joml", jomlVersion)
+    implementation("org.joml", "joml-primitives", `joml-primitivesVersion`)
 }
 
 tasks.test {
